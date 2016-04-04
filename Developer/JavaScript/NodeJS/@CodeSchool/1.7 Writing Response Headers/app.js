5b2e535d9c6087565811bdfc559532902a0e39d5
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-04 18:20:13 +0800

    NodeJS@CS 1.7 Done.

--- Developer/JavaScript/NodeJS/@CodeSchool/1.7 Writing Response Headers/app.js
+++ Developer/JavaScript/NodeJS/@CodeSchool/1.7 Writing Response Headers/app.js
@@ -2,7 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200);
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
 
   fs.readFile('index.html', function(err, contents) {
     response.write(contents);
