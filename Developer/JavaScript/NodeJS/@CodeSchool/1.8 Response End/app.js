8d2a8c80185072d4a33c76b9e2dc8acfed3bc17b
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-04 18:22:02 +0800

    NodeJS@CS 1.8 Init.

---
+++ Developer/JavaScript/NodeJS/@CodeSchool/1.8 Response End/app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+}).listen(8080);
