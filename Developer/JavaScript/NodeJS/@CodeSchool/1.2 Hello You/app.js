92539bfa0439cc8f1e91dceb862b2ffc8b9212fc
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-04 17:44:30 +0800

    NodeJS@CS 1.2 Done.

--- Developer/JavaScript/NodeJS/@CodeSchool/1.2 Hello You/app.js
+++ Developer/JavaScript/NodeJS/@CodeSchool/1.2 Hello You/app.js
@@ -1,5 +1,7 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-  
+  response.writeHead(200);
+  response.write("Hello, this is LucidCube.");
+  response.end();
 }).listen(8080);
