5b298b56f5c76d14b5510793f0cadc339c91940b
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-04 18:22:25 +0800

    NodeJS@CS 1.8 Done.

--- Developer/JavaScript/NodeJS/@CodeSchool/1.8 Response End/app.js
+++ Developer/JavaScript/NodeJS/@CodeSchool/1.8 Response End/app.js
@@ -2,6 +2,5 @@ var http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200);
-  response.write("Hello, this is dog");
-  response.end();
+  response.end("Hello, this is dog");
 }).listen(8080);
