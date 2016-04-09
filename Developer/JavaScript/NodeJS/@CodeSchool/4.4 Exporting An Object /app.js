ec99bb2e39fc80f99b4685ffee98f588a04867af
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-09 23:09:38 +0800

    NodeJS@CS 4.4 Done.

---
+++ Developer/JavaScript/NodeJS/@CodeSchool/4.4 Exporting An Object /app.js
@@ -0,0 +1,4 @@
+var logger = require('./logger');
+
+logger.info('This is some information');
+logger.warn('something bad is happening');
---
+++ Developer/JavaScript/NodeJS/@CodeSchool/4.4 Exporting An Object /logger.js
@@ -0,0 +1,11 @@
+exports.warn = function(message) {
+  console.log("Warning: " + message);
+};
+
+exports.info = function(message) {
+  console.log("Info: " + message);
+};
+
+exports.error = function(message) {
+  console.log("Error: " + message);
+};
