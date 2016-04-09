7f4d11fd0f860e4b82159c08c2859e1491b0257e
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-10 00:23:41 +0800

    NodeJS@CS 5.3 Done.

---
+++ Developer/JavaScript/NodeJS/@CodeSchool/5.3 Route Params/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+
+app.get('/quotes/:name', function(request, response) {
+  var name = request.params.name;
+  var quote = quotes[name];
+  response.end(quote);
+});
+
+app.listen(8080);
