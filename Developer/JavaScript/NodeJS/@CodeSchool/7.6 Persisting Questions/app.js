5749043267d5121b8729efe1ba17602682f5f3be
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-11 03:55:13 +0800

    NodeJS@CS 7.6 Done.

---
+++ Developer/JavaScript/NodeJS/@CodeSchool/7.6 Persisting Questions/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      // add the question to the list here
+      redisClient.lpush('questions', question);
+    }
+  });
+});
+ 
