154bd0b6959316bcef5f4b6723fd3f6416358e33
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-04-11 04:16:32 +0800

    NodeJS@CS 7.8 Done.

---
+++ Developer/JavaScript/NodeJS/@CodeSchool/7.8 Limiting Questions Stored/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+  redisClient.lrange("questions", 0, -1, function(err, questions) {
+    questions.forEach(function(question) {
+      client.emit("question", question);
+    });
+  });
+
+  client.on('answer', function(question, answer) {
+    client.broadcast.emit('answer', question, answer);
+  });
+
+  client.on('question', function(question) {
+    if(!client.question_asked) {
+      client.question_asked = true;
+      client.broadcast.emit('question', question);
+      redisClient.lpush("questions", question, function() {
+        redisClient.ltrim('questions', 0, 19);
+      });
+    }
+  }); 
+
+});
